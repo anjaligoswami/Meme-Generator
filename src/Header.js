26ef9b138fa3a97cea65a78c5f0bc419f08dc377
anjaligoswami <agcodes86400@gmail.com>
2021-12-09 21:48:15 +0530

    built

---
+++ src/Header.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+function Header() {
+    return (
+        <header>
+           <img src="https://i.pinimg.com/originals/2d/bd/7f/2dbd7feb530536368e994c944b2b9f8a.jpg" alt="Problem" 
+           />
+
+          <p>Meme Generator</p>
+
+        </header>
+    )
+ }
+ 
+ export default Header
